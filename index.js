258cb99897f46297f24fdc65f7aa39f7774b46ea
vinitsaspara <sasparavinit@gmail.com>
2025-05-15 17:25:46 +0530

    set vercel cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://job-frontend-v64g.vercel.app",
     credentials: true
 }
 
